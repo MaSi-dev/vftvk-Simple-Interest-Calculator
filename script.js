78d6e50997afad07eb179315890cec0347c892c7
Mohsen Asi <mohsen.asi@gmail.com>
2022-02-06 21:31:51 +0000

    change the logic and layout

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-function compute()
+function compute() // compute the final interest
 {
-    p = document.getElementById("principal").value;
-    
+    var principalIsCorrect = validation()
+    if(principalIsCorrect){ 
+        var principal = document.getElementById("principal").value;
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate / 100;
+        var year = new Date().getFullYear()+parseInt(years);
+        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year "+year+"\<br\>"    
+    }
+}
+function updateRate(){ // Update the field of rate
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
+function validation(){
+    var principalIsCorrect = true
+    var principleValue = document.getElementById("principal").value;
+    if(principleValue <= 0){ //alert the user to change the value
+        principalIsCorrect = false
+        alert("Enter a positive number!");
+        document.getElementById("principal").focus();
+    }
+    return principalIsCorrect
 }
         
