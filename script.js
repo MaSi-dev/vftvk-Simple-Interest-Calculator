2341715e0b12102b8fc5a8733a880b31cb102ae6
Mohsen <mohsen.asi87@.com>
2022-02-11 20:19:34 +0000

    minor change

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ function compute() // compute the final interest
         var years = document.getElementById("years").value;
         var interest = principal * years * rate / 100;
         var year = new Date().getFullYear()+parseInt(years);
-        document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year "+year+"\<br\>"    
+        document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"</mark>%\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"    
     }
 }
 function updateRate(){ // Update the field of rate
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText = rateval;
+    document.getElementById("rate_val").innerText = rateval + "%";
 }
 function validation(){
     var principalIsCorrect = true
